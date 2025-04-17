fb8f616efa4161968eb6e1f82d93a19520ffea01
Flavio1031 <flaviobarberbeiro@gmail.com>
2025-04-17 06:29:25 +0000

    novo committed

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,17 +9,34 @@ int main() {
      float pib1 = 73.0000;
      int pontos1 = 50;
     
-    
+     printf("carta:  01\n");
      printf(" nome da cidade:%s \n", cidade1);
      printf(" codigo da carta:%s \n", codigo1);
      printf("populaçao:%d \n", populacao1);
      printf(" área:%.2f \n", area);
      printf(" pib:%.2f \n", pib1);
      printf( "pontos turistico:%d \n" , pontos1);
-     printf("carta:  01\n");
 
 
 
+     printf("\n");
+     
+     char codigo2 []= "A02";
+     char cidade2 []= "Acre";
+     int populacao2 = 30900;
+     float area2 = 189.0;
+     float pib2 = 59.0000;
+     int pontos2 = 39;
+    
+     printf("carta:  02\n");
+     printf(" nome da cidade:%s \n", cidade2);
+     printf(" codigo da carta:%s \n", codigo2);
+     printf("populaçao:%d \n", populacao2);
+     printf(" área:%.2f \n", area2);
+     printf(" pib:%.2f \n", pib2);
+     printf( "pontos turistico:%d \n" , pontos2);
+     
+     
      return 0;
 
 }
